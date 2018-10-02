996ec7c1fdf156ee32e18de5368128b9b38daa0c
JAC <jabellan@systra.com>
2018-10-02 11:25:40 +0100

    apply flake8

--- lin_parser.py
+++ lin_parser.py
@@ -3,30 +3,31 @@
 import re
 import ast
 
+
 class node:
     """
     Simple node class.
     """
-    
+
     def __init__(self, string, **kwargs):
         string = str(string)
         self.ID = abs(int(string))
         self.__dict__.update(**kwargs)
-        
+
         if string.startswith('-'):
             self.stopping = False
         else:
             self.stopping = True
-    
+
     @property
     def attrs(self):
         '''Returns a dictionary with user-defined attributes.'''
-        return {k:v for k,v in self.__dict__.items()
-                if '_' not in k and 'ID' != k and 'stopping' != k }
-    
+        return {k: v for k, v in self.__dict__.items()
+                if '_' not in k and 'ID' != k and 'stopping' != k}
+
     def __str__(self):
         return '{}{}, {}'.format('-' if not self.stopping else '', self.ID,
-                self.attrs)
+                                 self.attrs)
 
 
 class line:
@@ -34,7 +35,7 @@ class line:
     Simple line class. A line is a sequence of nodes, but each line can have
     it's own properties.
     """
-    
+
     def __init__(self, nodes, **kwargs):
         self.nodes = nodes  # a list of node objects
         self.__dict__.update(**kwargs)
@@ -42,7 +43,7 @@ class line:
     @property
     def attrs(self):
         '''Returns a dictionary with user-defined attributes.'''
-        return {k:v for k,v in self.__dict__.items()
+        return {k: v for k, v in self.__dict__.items()
                 if '_' not in k and 'nodes' != k}
 
     @property
@@ -55,7 +56,7 @@ class line:
                 len(self.nodes), len(self.stops))
         msg += f'\n{self.attrs}'
         return msg
-    
+
     @staticmethod
     def from_string(string):
 
@@ -67,33 +68,34 @@ class line:
         nodes = []
         line_attrs = {}
         node_attrs = {}
-        
+
         n = None
         attrs_section = True
-        
+
         while parts:
             p = parts.pop(0)
-            
+
             if bool(re.search('N(?:ODES)?\s*=', p)):
                 attrs_section = False
-            
+
             if attrs_section:
                 k, _, v = p.partition('=')
-                try:
-                    line_attrs.update({k.strip(): ast.literal_eval(v.strip())})
-                except:
-                    line_attrs.update({k.strip(): v.strip()})
-                
+                if k and v:
+                    try:
+                        line_attrs.update({k.strip(): ast.literal_eval(v)})
+                    except ValueError:
+                        line_attrs.update({k.strip(): v.strip()})
+
             else:
                 p = re.sub('^\s*N(?:ODES)?\s*=\s*', '', p)
-                
+
                 if '=' in p:
                     k, _, v = p.partition('=')
-                    try:
-                        node_attrs.update({k.strip(): ast.literal_eval(v.strip())})
-                    except:
-                        node_attrs.update({k.strip(): v.strip()})
-                
+                    if k and v:
+                        try:
+                            node_attrs.update({k.strip(): ast.literal_eval(v)})
+                        except ValueError:
+                            node_attrs.update({k.strip(): v.strip()})
                 else:
                     if n:
                         # Add the previous node, with the attrs read so far
@@ -103,22 +105,22 @@ class line:
                         if not parts:
                             # Add the last node if there are no more attrs
                             nodes.append(node(p))
-                    
+
                     n = p  # Set the new node for future attrs to be read
-        
-        l = line(nodes, **line_attrs)
-        
-        return l
+
+        ln = line(nodes, **line_attrs)
+
+        return ln
 
 
 class system:
     """
     Simple system class. A system is a number of lines.
     """
-    
+
     def __init__(self, lines):
         self.lines = lines
-    
+
     def __str__(self):
         msg = f'System with {len(self.lines.keys())} lines.'
         if hasattr(self, 'comments'):
@@ -128,40 +130,39 @@ class system:
     @staticmethod
     def _extract_lines(string, line_pat=r'(?s)LINE (.*?)(?=LINE|;|\Z)'):
         line_re = re.compile(line_pat)
-        
+
         records = line_re.findall(string)
         lines = set([line.from_string(r) for r in records])
         # Assumes all lines have a NAME, and that this is unique.
         lines = {l.NAME: l for l in lines}
-        
+
         return lines
-    
+
     @staticmethod
     def _extract_comments(string, comment_pat=r'(?s);.*?(?=LINE|\Z)'):
         comment_re = re.compile(comment_pat)
-        
+
         comments = comment_re.findall(string)
-        
+
         return comments
-    
+
     @staticmethod
     def from_string(string):
 
         lines = system._extract_lines(string)
         comments = system._extract_comments(string)
-        
+
         s = system(lines)
         if comments:
             s.comments = comments
-        
+
         return s
-    
+
     @staticmethod
     def read_file(path):
         with open(path, 'r') as ifile:
             content = ifile.read()
-        
-        s = system.from_string(content)
-        
-        return s
 
+        s = system.from_string(content)
+
+        return s
